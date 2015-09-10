28074357c04811d577ed440ba874953c558e2759
Jason Pilz <jasonpilz@gmail.com>
2015-09-09 21:31:44 -0600

    Refactored NightWrite

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -2,36 +2,36 @@ require 'pry'
 require_relative 'braille_map'
 
 class NightWrite
+  attr_accessor :first, :second, :third, :input_file, :output_file
 
-  def self.to_braille(file)
-    characters = file.chars
+  def initialize(input, output)
+    @input_file = input
+    @output_file = output
+    @first = ""
+    @second = ""
+    @third = ""
+  end
 
-    first = ""
-    second = ""
-    third = ""
+  def create_file
+    handle = File.read(@input_file) # TODO Verify the handle.chars.count <= 40
+    file = handle.downcase.strip # TODO remove downcase for capitalization
+    File.write(output_file, to_braille(file))
+    output_message(file)
+  end
 
-    characters.each do |char|
-      first << BrailleMap::KEYS.fetch(char)[0]
-      second << BrailleMap::KEYS.fetch(char)[1]
-      third << BrailleMap::KEYS.fetch(char)[2]
+  def to_braille(file)
+    file.chars.each do |char|
+      @first << BrailleMap::KEYS.fetch(char)[0]
+      @second << BrailleMap::KEYS.fetch(char)[1]
+      @third << BrailleMap::KEYS.fetch(char)[2]
     end
+    "#{@first}\n#{@second}\n#{@third}"
+  end
 
-    # Return 3 Strings for every 1 string provided
-    "#{first}\n#{second}\n#{third}"
+  def output_message(file)
+    puts "Created #{output_file}, containing #{file.chars.count} characters"
   end
 
 end
 
-
-input_file = ARGV[0]
-output_file = ARGV[1]
-
-handle = File.read(input_file)
-
-# TODO Verify the handle.chars.count <= 40
-
-braille = NightWrite.to_braille(handle.downcase.strip)
-
-File.write(output_file, braille)
-
-puts "Created #{output_file} containing #{handle.strip.chars.count} characters"
+NightWrite.new(ARGV[0], ARGV[1]).create_file
